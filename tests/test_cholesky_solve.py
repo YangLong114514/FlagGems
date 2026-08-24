cbb81fd464ee7bd9b8b649286d7c681462f25609
YangLong114514 <long.yang@compiler-dev.com>
2026-08-24 10:03:47 +0800

    [KMCompiler] Build Ascend conditioned inputs on CPU

--- tests/test_cholesky_solve.py
+++ tests/test_cholesky_solve.py
@@ -141,7 +141,7 @@ def _make_cholesky_solve_broadcast_inputs(A_shape, rhs_shape, dtype, upper=False
 
 def _make_conditioned_inputs(shape, dtype):
     *batch_dims, n, nrhs = shape
-    build_on_cpu = _use_cpu_complex_path(dtype)
+    build_on_cpu = IS_ASCEND or _use_cpu_complex_path(dtype)
     setup_device = "cpu" if build_on_cpu else flag_gems.device
     is_single_precision = dtype in (torch.float32, torch.complex64)
     real_dtype = torch.float32 if is_single_precision else torch.float64
